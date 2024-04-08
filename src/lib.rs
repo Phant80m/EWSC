061bd125b2f52d6434aaacb2a1bc2ac27c9311a2
phant80m <black.pandazayd@gmail.com>
2024-04-08 20:45:08 +1000

    ewsc

---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+#[macro_export]
+macro_rules! warning {
+    ($message:expr) => {
+        use owo_colors::OwoColorize;
+        println!(
+            "{} {}",
+            "[  ]".yellow().bold(),
+            $message
+        );
+    };
+    ($message:expr, $($arg:expr),*) => {
+        use owo_colors::OwoColorize;
+        println!(
+            "{} {}",
+            "[  ]".yellow().bold(),
+            format!($message, $($arg),*)
+        );
+    };
+}
+#[macro_export]
+macro_rules! error {
+    ($message:expr) => {
+        use owo_colors::OwoColorize;
+        eprintln!(
+            "{} {}",
+            "[  ]".red().bold(),
+            $message
+        );
+    };
+    ($message:expr, $($arg:expr),*) => {
+        use owo_colors::OwoColorize;
+        eprintln!(
+            "{} {}",
+            "[  ]".red().bold(),
+            format!($message, $($arg),*)
+        );
+    };
+}
+#[macro_export]
+macro_rules! success {
+    ($message:expr) => {
+        use owo_colors::OwoColorize;
+        println!(
+            "{} {}",
+            "[  ]".green().bold(),
+            $message
+        );
+    };
+    ($message:expr, $($arg:expr),*) => {
+        use owo_colors::OwoColorize;
+        println!(
+            "{} {}",
+            "[  ]".green().bold(),
+            format!($message, $($arg),*)
+        );
+    };
+}
+#[macro_export]
+macro_rules! cmd {
+    ($command:expr) => {{
+        use std::process::{Command, Stdio};
+        use $crate::error;
+
+        let mut command = Command::new(&$command[0]);
+        for arg in &$command[1..] {
+            command.arg(arg);
+        }
+
+        let cmd = command
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn();
+
+        match cmd {
+            Ok(mut child) => {
+                let status = child.wait();
+                if let Err(err) = status {
+                    error!("Error waiting for process: {}", err.red().bold());
+                }
+            }
+            Err(err) => {
+                error!("Error spawning process: {}", err.red().bold());
+            }
+        }
+    }};
+}
